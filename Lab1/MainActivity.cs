6e8447a581982cbc605bc8ec8ebe38a7e4fe0b31
Zulu <jzuluaga55@gmail.com>
2017-05-24 13:53:32 -0500

    Finish lab 01

---
+++ Lab1/MainActivity.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Lab1
+{
+    [Activity(Label = "Lab1", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        Button button;
+        TextView textViewDev;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            button = FindViewById<Button>(Resource.Id.MyButton);
+            textViewDev = FindViewById<TextView>(Resource.Id.textViewDev);
+
+            button.Click += Button_Click;
+        }
+
+        private async void Button_Click(object sender, EventArgs e)
+        {
+            textViewDev.Text = "Juan Carlos Zuluaga Cardona";
+            string myDevice = Android.Provider.Settings.Secure.GetString(ContentResolver, Android.Provider.Settings.Secure.AndroidId);
+            XamarinDiplomado.ServiceHelper helper = new XamarinDiplomado.ServiceHelper();
+            await helper.InsertarEntidad("jzuluaga55@gmail.com", "lab1", myDevice);
+            button.Text = "Gracias por completar el Lab1";
+        }
+    }
+}
+
